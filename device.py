d90e27345fa2271320b474464b2258e6bddc1265
raven703 <93988872+raven703@users.noreply.github.com>
2022-06-29 12:52:13 +0300

    Add files via upload

---
+++ device.py
@@ -0,0 +1,40 @@
+from machine import Pin
+
+
+class motorDevice():
+    def __init__(self, pin_number) -> None:
+
+        self.pinControl = Pin(pin_number,Pin.OUT) #set pin for control
+        self.pinNumber = pin_number
+        self.pinControl.off()
+        pass
+
+    def start(self):
+        #print("start in pin = ", self.pinNumber)
+        self.pinControl.on()
+        if self.pinControl.value() == 1:
+            return True
+        else:
+            return False
+        
+    def stop(self):
+        #print("stop in pin = ", self.pinNumber)
+        self.pinControl.off()
+        if self.pinControl.value() == 0:
+            return True
+        else:
+            return False
+        
+    def status(self):
+        if self.pinControl.value() == 1:
+            return [True, "Running"]
+        else:
+            return [False, "Stopped"]
+
+    
+    def value(self):
+        return self.pinControl.value()
+
+        
+
+
--- main.py
+++ main.py
@@ -1,23 +1,65 @@
 import uasyncio as asyncio
+import urequests
+
 from microdot_asyncio import Microdot, redirect, send_file, Response
-from machine import Pin, SoftI2C as I2C
+from machine import Pin, SoftI2C as I2C, RTC
 from htu21d import *
 from esp8266_i2c_lcd import I2cLcd
+from device import *
 
+rtc = RTC()
+autoWork = True
 
+def setTime():
+    url = "http://worldtimeapi.org/api/timezone/Europe/Moscow"
+    response = urequests.get(url)
+    if response.status_code == 200:
+         datetime_str = str(response.json()["datetime"])
+         year = int(datetime_str[0:4])
+         month = int(datetime_str[5:7])
+         day = int(datetime_str[8:10])
+         hour = int(datetime_str[11:13])
+         minute = int(datetime_str[14:16])
+         second = int(datetime_str[17:19])
+         subsecond = int(round(int(datetime_str[20:26]) / 10000))
+
+         rtc.datetime((year, month, day, 0, hour, minute, second, subsecond))
+         date_str = "Date: {2:02d}.{1:02d}.{0:4d}".format(*rtc.datetime())
+         time_str = "Time: {4:02d}:{5:02d}:{6:02d}".format(*rtc.datetime())
+         print(date_str)
+         print(time_str)
+         return True
+         
+    else:
+        print("Time is not set")
+        return False 
+
+def getRtcTime():
+    date_str = "Date: {2:02d}.{1:02d}.{0:4d}".format(*rtc.datetime())
+    time_str = "Time: {4:02d}:{5:02d}:{6:02d}".format(*rtc.datetime())
+    return [date_str, time_str]
+        
+        
 # setup webserver
 app = Microdot()
+setTime()
+
 # 
 gy21 = HTU21D(22,21)  #take temp and hum from sensor
+fan = motorDevice(12)
+pump = motorDevice(14)
+print("FAN is: ", fan.status())
+print("PUMP is: ", pump.status())
 
 pumpControlPin = Pin(14,Pin.OUT)
+soilMeter = Pin(13,Pin.IN)
 fanControlPin = Pin(12,Pin.OUT)
 
 pumpControlPin.off()
 fanControlPin.off()
 
 
-async def printTemper():
+async def writeLog():
     
     while True: 
         
@@ -27,10 +69,42 @@ async def printTemper():
             temper = str(round(gy21.temperature))
             hum = str(round(gy21.humidity))
             #print (f.tell(), "pos=", pos)
+            #print(f"Temp: {temper} RH: {hum}")       
             f.write(temper + "," + hum + ",")
-            print(f"Temp: {temper} RH: {hum}")
             f.close()
             await asyncio.sleep(3000)
+
+async def deviceControl():
+    write_request = True
+    last_max_temper = 0
+    while True: 
+        
+        temper = round(gy21.temperature,1)
+        hum = round(gy21.humidity,1)
+        if temper < 30: last_max_temper = 30
+        if autoWork and temper > 30:
+          
+            if temper > last_max_temper:
+                last_max_temper = temper
+                write_request = True
+            
+            if not fan.status()[0]: fan.start()
+            if write_request:
+                with open("log.txt", "a+") as output:
+                    output.write(f'Temp: {temper} {getRtcTime()} \n')
+                   
+                    if round(gy21.temperature,1) > last_max_temper: 
+                        write_request = True
+                       
+                    else:
+                        write_request = False
+                      
+        else:
+            if fan.status()[0]: fan.stop()
+            
+            write_request = True
+        
+        await asyncio.sleep(5)
     
 @app.route('/')
 async def hello(response):
@@ -111,8 +185,10 @@ async def shutdown(request):
 
 async def main():
     task1 = asyncio.create_task(app.start_server(port=80, debug=False))
-    task2 = asyncio.create_task(printTemper())
+    task2 = asyncio.create_task(writeLog())
+    task3 = asyncio.create_task(deviceControl())
     await task1
     await task2
+    await task3
     
 asyncio.run(main())
